22e0bab67bcb945bf838000cfcb4979d99aab4e5
Teemu <Teemu@LAPTOP-TK92GG3E>
2018-10-30 13:43:22 +0200

    uusicommit

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -6,3 +6,11 @@ while number !=0:
     number = int(input("Enter next number\n"))
 print ("You entered", counter, "numbers")
 print ("asdggteemu")
+
+#Example
+int_list = [100, 992, 3, 4, 5, 6]
+sum = 0
+for iter in int_list:
+    sum += iter
+print("Sum =", sum)
+print("Avg =", sum/len(int_list))
