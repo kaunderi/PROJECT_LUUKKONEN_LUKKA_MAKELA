2588cbbfad481fbb5bcf8fb4a7c869fc4ce0a185
Teemu <Teemu@LAPTOP-TK92GG3E>
2018-10-30 13:04:49 +0200

    dsfsd

---
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,8 @@
+counter = 0
+number = int(input("Enter first number"))
+while number !=0:
+    if number > 0:
+        counter +=1
+    number = int(input("Enter next number\n"))
+print ("You entered", counter, "numbers")
+print ("NONIINNN")
