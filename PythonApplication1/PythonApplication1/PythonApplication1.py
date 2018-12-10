106c61f57d38ac560616cba1d4161a5dc636aee6
Teme54 <33248988+Teme54@users.noreply.github.com>
2018-12-10 11:40:29 +0200

    Delete PythonApplication1.py

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++
@@ -1,8 +0,0 @@
-counter = 0
-number = int(input("Enter first number"))
-while number !=0:
-    if number > 0:
-        counter +=1
-    number = int(input("Enter next number\n"))
-print ("You entered", counter, "numbers")
-print ("asdggteemu")
